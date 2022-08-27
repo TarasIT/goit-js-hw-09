805c316fa33ed81bb6e39a77d30b67be10deef1d
Taras Maltsev <98047270+TarasIT@users.noreply.github.com>
2022-08-27 19:13:55 +0300

    3rd task is done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,58 @@
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+  firstDelay: document.querySelector('[name=delay]'),
+  delayStep: document.querySelector('[name=step]'),
+  amount: document.querySelector('[name=amount]'),
+};
+
+refs.form.addEventListener('submit', onBtnCreatePromisesClick);
+
+function onBtnCreatePromisesClick(event) {
+  event.preventDefault();
+
+  const delayStep = parseInt(refs.delayStep.value);
+  const amount = parseInt(refs.amount.value);
+  let delay = parseInt(refs.firstDelay.value);
+  let position = 0;
+
+  const createPromises = setInterval(() => {
+    position += 1;
+
+    if (position !== 1) {
+      delay += delayStep;
+    }
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+
+    if (position === amount) {
+      clearInterval(createPromises);
+    }
+  }, delayStep);
+
+  event.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
 }
